61e1b4ae8efea6a95626ad39adca46b39a4dbcd0
Steven <asongala@163.com>
2022-07-08 12:01:15 +0800

    Create database if non exists in migrator and update frontend Dockerfile (#13)
    * Create database if non exists in migrator.
    
    * Build frontend as static. Update Dockerfile of frontend.

--- backend/src/db/migrator.rs
+++ backend/src/db/migrator.rs
@@ -1,12 +1,19 @@
 use crate::Settings;
 use anyhow::Result;
-use sqlx::migrate::Migrator;
-use sqlx::postgres::PgPool;
+use sqlx::migrate::{MigrateDatabase, Migrator};
+use sqlx::postgres::{PgPool, Postgres};
 
 static MIGRATOR: Migrator = sqlx::migrate!("db/migrations");
 
 pub async fn run() -> Result<()> {
-    let db_pool = PgPool::connect(Settings::get().db_url.as_str()).await?;
+    // Create database if non existing.
+    let db_url = &Settings::get().db_url.as_str();
+    if !Postgres::database_exists(db_url).await? {
+        Postgres::create_database(db_url).await?;
+    }
+
+    // Run migrations.
+    let db_pool = PgPool::connect(db_url).await?;
     MIGRATOR.run(&db_pool).await?;
     Ok(())
 }
