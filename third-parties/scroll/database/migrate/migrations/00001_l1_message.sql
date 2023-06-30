cb1fe4ea9203b878807f6a7f87de8c8ad8e6406d
Steven Gu <asongala@163.com>
2023-06-30 20:20:44 +0800

    Add new DB migrations (including `chunk`).

--- third-parties/scroll/database/migrate/migrations/00001_l1_message.sql
+++ third-parties/scroll/database/migrate/migrations/00001_l1_message.sql
@@ -13,12 +13,12 @@ create table l1_message
     layer1_hash  VARCHAR NOT NULL,
     layer2_hash  VARCHAR DEFAULT NULL,
     status       INTEGER  DEFAULT 1,
-    created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 comment
-on column l1_message.status is 'undefined, pending, submitted, confirmed, failed, expired';
+on column l1_message.status is 'undefined, pending, submitted, confirmed, failed, expired, relay_failed';
 
 create unique index l1_message_hash_uindex
 on l1_message (msg_hash);
@@ -28,19 +28,6 @@ on l1_message (queue_index);
 
 create index l1_message_height_index
     on l1_message (height);
-
-CREATE OR REPLACE FUNCTION update_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-   NEW.updated_time = CURRENT_TIMESTAMP;
-   RETURN NEW;
-END;
-$$ language 'plpgsql';
-
-CREATE TRIGGER update_timestamp BEFORE UPDATE
-ON l1_message FOR EACH ROW EXECUTE PROCEDURE
-update_timestamp();
-
 
 -- +goose StatementEnd
 
