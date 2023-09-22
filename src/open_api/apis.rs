457899b6d0281442c1f9144ac3c8ce52a0e129b9
Steven <asongala@163.com>
2023-09-22 15:48:33 +0800

    feat: add `prometheus` to `/metrics` router (#149)
    * Add prometheus to route `/metrics`.
    
    * Add `tokio_metrics`.

--- src/open_api/apis.rs
+++ src/open_api/apis.rs
@@ -1,11 +1,11 @@
 use crate::{
     consts::*,
     db::*,
-    open_api::{responses::*, State},
+    open_api::{responses::*, State, INCOMING_REQUESTS, RESPONSE_TIME_COLLECTOR},
 };
 use poem::{error::InternalServerError, web::Data, Result};
 use poem_openapi::{param::Query, payload::Json, OpenApi};
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 
 // Macro used to log error with right line number.
 macro_rules! api_err {
@@ -21,12 +21,20 @@ pub(crate) struct Apis;
 impl Apis {
     #[oai(path = "/batch", method = "get")]
     async fn batch(&self, state: Data<&State>, index: Query<i64>) -> Result<Json<BatchResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let index = index.0;
 
         // Return directly if cached.
         let cache_key = format!("batch-{index}");
         if let Some(response) = BatchResponse::from_cache(state.cache.as_ref(), &cache_key).await {
             log::debug!("OpenAPI - Get batch from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["batch"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -48,6 +56,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["batch"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -58,6 +70,9 @@ impl Apis {
         page: Query<Option<u64>>,
         per_page: Query<Option<u64>>,
     ) -> Result<Json<BatchesResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let limit = per_page.0.map_or_else(
             || DEFAULT_PER_PAGE,
             |val| {
@@ -81,6 +96,11 @@ impl Apis {
         if let Some(response) = BatchesResponse::from_cache(state.cache.as_ref(), &cache_key).await
         {
             log::debug!("OpenAPI - Get batches from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["batches"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -105,6 +125,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["batches"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -114,12 +138,20 @@ impl Apis {
         state: Data<&State>,
         batch_index: Query<i64>,
     ) -> Result<Json<BlocksResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let batch_index = batch_index.0;
 
         // Return directly if cached.
         let cache_key = format!("blocks-of-batch-{batch_index}");
         if let Some(response) = BlocksResponse::from_cache(state.cache.as_ref(), &cache_key).await {
             log::debug!("OpenAPI - Get blocks from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["batch_blocks"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -138,6 +170,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["batch_blocks"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -147,12 +183,20 @@ impl Apis {
         state: Data<&State>,
         batch_index: Query<i64>,
     ) -> Result<Json<ChunksResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let batch_index = batch_index.0;
 
         // Return directly if cached.
         let cache_key = format!("chunks-of-batch-{batch_index}");
         if let Some(response) = ChunksResponse::from_cache(state.cache.as_ref(), &cache_key).await {
             log::debug!("OpenAPI - Get chunks from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["chunks"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -184,6 +228,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["chunks"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -193,12 +241,20 @@ impl Apis {
         state: Data<&State>,
         chunk_index: Query<i64>,
     ) -> Result<Json<BlocksResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let chunk_index = chunk_index.0;
 
         // Return directly if cached.
         let cache_key = format!("blocks-of-chunk-{chunk_index}");
         if let Some(response) = BlocksResponse::from_cache(state.cache.as_ref(), &cache_key).await {
             log::debug!("OpenAPI - Get blocks from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["chunk_blocks"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -217,6 +273,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["chunk_blocks"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -225,11 +285,19 @@ impl Apis {
         &self,
         state: Data<&State>,
     ) -> Result<Json<LastBatchIndexesResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         // Return directly if cached.
         if let Some(response) =
             LastBatchIndexesResponse::from_cache(&state.cache, "last_batch_indexes").await
         {
             log::debug!("OpenAPI - Get last batch indexes from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["last_batch_indexes"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -251,6 +319,10 @@ impl Apis {
             log::error!("OpenAPI - Failed to save cache for last batch indexes: {error}");
         }
 
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["last_batch_indexes"])
+            .observe(response_time.elapsed().as_secs_f64());
+
         Ok(Json(response))
     }
 
@@ -262,12 +334,20 @@ impl Apis {
         state: Data<&State>,
         keyword: Query<String>,
     ) -> Result<Json<SearchResponse>> {
+        let response_time = Instant::now();
+        INCOMING_REQUESTS.inc();
+
         let keyword = keyword.0;
 
         // Return directly if cached.
         let cache_key = format!("search-{keyword}");
         if let Some(response) = SearchResponse::from_cache(state.cache.as_ref(), &cache_key).await {
             log::debug!("OpenAPI - Get blocks from Cache: {response:?}");
+
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["search"])
+                .observe(response_time.elapsed().as_secs_f64());
+
             return Ok(Json(response));
         };
 
@@ -304,6 +384,10 @@ impl Apis {
         {
             log::error!("OpenAPI - Failed to save cache of {cache_key}: {error}");
         }
+
+        RESPONSE_TIME_COLLECTOR
+            .with_label_values(&["search"])
+            .observe(response_time.elapsed().as_secs_f64());
 
         Ok(Json(response))
     }
