b390d0ce1e3cd43ac67a88b457ed5684c0033180
georgehao <haohongfan@gmail.com>
2023-09-26 16:19:12 +0800

    Update src/consts.rs
    Co-authored-by: colin <102356659+colinlyguo@users.noreply.github.com>

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,7 @@
 // Expired seconds of cache data.
 pub const DEFAULT_CACHE_EXPIRED_SECS: u64 = 15;
 
-// Query parameter `page` starts from `1`, default `per_page` is 20, and max value is 100
+// Query parameter `page` starts from `1`, default `per_page` is 10.
 // (configured in .env).
 pub const DEFAULT_PER_PAGE: u64 = 10;
 
