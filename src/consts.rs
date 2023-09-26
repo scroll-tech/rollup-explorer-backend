8df355b1cfd1273947b57c49895092b0c1c61c6c
georgehao <haohongfan@gmail.com>
2023-09-26 16:17:35 +0800

    feat: update

--- src/consts.rs
+++ src/consts.rs
@@ -3,7 +3,7 @@ pub const DEFAULT_CACHE_EXPIRED_SECS: u64 = 15;
 
 // Query parameter `page` starts from `1`, default `per_page` is 20, and max value is 100
 // (configured in .env).
-pub const DEFAULT_PER_PAGE: u64 = 20;
+pub const DEFAULT_PER_PAGE: u64 = 10;
 
 // Identify if batch or chunk is invalid.
 pub const INVALID_INDEX: i64 = -1;
